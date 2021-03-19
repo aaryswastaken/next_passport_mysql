cbce1dd9b42a6a9880a17097bdbed944a80208a7
vsahler <vincent.sahler@gmail.com>
2021-03-19 19:41:22 +0100

    Cleanup

--- tests/authentication.test.js
+++ tests/authentication.test.js
@@ -1,44 +1,5 @@
 import { createUser, getUser, deleteUser, validatePassword } from "../lib/user.js"
-import compose from "docker-compose"
 import mysql from "mysql";
-
-// THIS IS A DIRTY WAY TO SAVE AND RESTORE THE DB
-
-var tablesToClean = [
-    "user"
-]
-var dump = {};
-
-/* beforeAll((done) => {
-    let n = 0;
-
-    tablesToClean.forEach((tableName) => {
-        conn.query(`SELECT * from ${tableName}`, (err, results, fields) => {
-            dump[tableName] = results;
-
-            console.log("Before delete");
-
-            conn.query(`DELETE FROM ${tableName}`, (err, results, fields) => {
-                n+=1;
-                console.log("Deleted "+tableName);
-                if(n === tablesToClean.length) {
-                    done();
-                }
-            });
-        });
-    });
-})
-
-afterAll(() => {
-    tablesToClean.forEach((tableName) => {
-        conn.query(`DELETE FROM ${tableName}`);
-
-        dump[tableName].forEach((row) => {
-            conn.query(`INSERT INTO ${tableName} SET ?`, row)
-        })
-    });
-}) */
-
 
 // Implement test server
 beforeAll((done) => {
